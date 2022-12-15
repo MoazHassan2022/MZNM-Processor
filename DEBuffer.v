1fd61fcfd8cf71d3560b6497a302107d74d34ada
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2022-12-15 05:01:23 +0200

    edit control unit and add new Buffer DE

--- DEBuffer.v
+++ DEBuffer.v
@@ -1,33 +1,43 @@
-module DEBuffer(ST,SST,Reg1,Reg2,Instruction,SrcAddress,RegDestination,Clk, STOut, SSTOut, Reg1Out, Reg2Out, InstructionOut, SrcAddressOut, RegDestinationOut);
+module DEBuffer(ST,SST,Reg1,Reg2,Instruction,SrcAddress,RegDestination,Clk, STOut, SSTOut, Reg1Out, Reg2Out, 
+InstructionOut, SrcAddressOut, RegDestinationOut,FlashNumIn,FlashNumOut);
+
+
 // Inputs to the buffer
 input Clk;
-input ST,SST;
+input ST;
+input SST;
 input [16:0] Reg1,Reg2;
 input [4:0]  Instruction;
 input [2:0]  SrcAddress;
 input [2:0]  RegDestination;
+input [2:0]  FlashNumIn;
+
+
 // Outputs from the buffer
-output reg   STOut,SSTOut;
+output reg   STOut;
+output reg   SSTOut;
 output reg  [16:0] Reg1Out;
 output reg  [16:0] Reg2Out;
 output reg  [4:0]  InstructionOut;
 output reg  [2:0]  SrcAddressOut;
 output reg  [2:0]  RegDestinationOut;
+output reg  [2:0]  FlashNumOut;
 
 
 // Regs in the buffer
-reg STReg,SSTReg;
+reg STReg;
+reg SSTReg;
+reg [2:0] FlashNumReg;
 reg [16:0] Reg1Reg,Reg2Reg;
 reg [4:0]  InstructionReg;
 reg [2:0]  SrcAddressReg;
 reg [2:0]  RegDestinationReg;
 
-// reg  STOut,SSTOut;
-// reg [16:0] Reg1Out,Reg2Out;
-// reg [4:0]  InstructionOut;
-// reg [2:0]  SrcAddressOut;
-// reg [2:0]  RegDestinationOut;
 
+// will read the St and SST at any time 
+assign STOut=STReg;
+assign SSTOut=SSTReg;
+assign FlashNumOut=FlashNumReg;
 always@(posedge Clk)
 begin
     STReg=ST;
@@ -41,8 +51,7 @@ end
 
 always@(negedge Clk)
 begin
-    STOut=STReg;
-    SSTOut=SSTReg;
+    FlashNumReg=FlashNumIn;
     Reg1Out=Reg1Reg;
     Reg2Out=Reg2Reg;
     InstructionOut=InstructionReg;
