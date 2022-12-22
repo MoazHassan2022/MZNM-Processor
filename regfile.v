57059894d77bda3c6394d4d5aea63eb3a248eea6
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2022-12-22 11:37:35 +0200

    Putting #0 delay before write in regFile

--- regfile.v
+++ regfile.v
@@ -26,7 +26,7 @@ begin
     end
     else
     begin
-        #1; // TODO: try to avoid doing this making M2W buffer makes registersOut = inputs @negedge clk
+        #0; // TODO: try to avoid doing this making M2W buffer makes registersOut = inputs @negedge clk
         if(write_enable) begin
             registers[write_addr] = write_data;
         end
